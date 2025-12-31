51b9d7dd1202b885c560828cf9572105b340d635
Qin <mutaqinsyahril07@gmail.com>
2025-12-31 19:47:25 +0700

    baru

--- src/datatype.kt
+++ src/datatype.kt
@@ -4,3 +4,19 @@
 val charA: Char = 'A'
 val charB: Char = 'B'
 val charUnicode: Char = '\u0041' // Unicode untuk 'A'
+
+// boolean
+val isTrue: Boolean = true
+val isFalse: Boolean = false
+
+// tipe string
+
+val greeting: String = "Hello, World!"
+val multilineString: String = """
+    This is a multiline
+    string in Kotlin.
+""".trimIndent()
+
+fun main() {
+    println(multilineString)
+}
